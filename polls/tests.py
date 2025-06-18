961704e09ca9fab478eb87e4fc5ec4d53a9a7059
Moisès <mgome298@xtec.cat>
2025-06-18 16:48:22 +0000

    afegit un nou test

--- polls/tests.py
+++ polls/tests.py
@@ -3,3 +3,7 @@ from django.test import TestCase
 class TestSenzillet(TestCase):
     def test_veritat_vertadera(self):
         self.assertTrue(True)
+
+    def test_falsetat_falsa(self):
+        self.assertFalse(False)
+
